9c698d09fc909bbf384955975a89eec12dff950b
Kyjo5975 <kyjo5975@colorado.edu>
2022-02-03 13:23:15 -0700

    Added in files for the ICI test and the resulting data. The ICI test is a representative test of what ENTICE will be attempting to do.

---
+++ HIRS_test.py
@@ -0,0 +1,3 @@
+from pyarts.workspace import Workspace
+ws = Workspace()
+ws.execute_controlfile("instruments/hirs/TestHIRS_fast.arts")
---
+++ test2.py
@@ -0,0 +1,6 @@
+from pyarts.workspace import Workspace
+ws = Workspace()
+ws.execute_controlfile("general/general.arts")
+ws.execute_controlfile("general/continua.arts")
+ws.execute_controlfile("general/agendas.arts")
+ws.execute_controlfile("general/planet_earth.arts")
